73cc7e269e0ca7b82287c93f9738222d9783ac48
domephant <dominikschulz@gmx.net>
2022-04-15 23:26:39 +0200

    Added missing sudos

--- setup_fedora.sh
+++ setup_fedora.sh
@@ -44,8 +44,8 @@ install_apps() {
     # Necessary imports
 
     # VS Code
-    rpm --import https://packages.microsoft.com/keys/microsoft.asc
-    sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+    echo $PASSWORD | sudo -S rpm --import https://packages.microsoft.com/keys/microsoft.asc
+    echo $PASSWORD | sudo -S sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
     echo $PASSWORD | sudo -S dnf check-update
 
     # Steam
@@ -81,7 +81,6 @@ install_apps() {
     # Microsoft Edge
     if [[$EDGE == "true"]]; then 
         echo $PASSWORD | sudo -S dnf install dnf-plugins-core -y
-        rpm --import https://packages.microsoft.com/keys/microsoft.asc
         echo $PASSWORD | sudo -S dnf config-manager --add-repo https://packages.microsoft.com/yumrepos/edge
         echo $PASSWORD | sudo -S dnf update --refresh -y
         echo $PASSWORD | sudo -S dnf install microsoft-edge-stable -y
