704957ba4c6a6f051ad438c2d9911827f6ecbaae
domephant <dominikschulz@gmx.net>
2022-04-16 11:05:25 +0200

    Fixed issues regarding input processing

--- setup_fedora.sh
+++ setup_fedora.sh
@@ -12,6 +12,39 @@ EDGE="false"
 VIM_NANO="false"
 POP="false"
 
+# echo $PASSWORD | sudo -S password input
+
+echo "Enter sudo password:"
+read -s PASSWORD
+
+# Command line arguments processing
+
+for var in "$@"; do
+    case "$var" in 
+        --spotify) 
+            SPOTIFY="true"
+        ;;
+        --postman) 
+            POSTMAN="true"
+        ;;
+        --thunderbird) 
+            THUNDERBIRD="true"
+        ;;
+        --chrome)
+            CHROME="true"
+        ;;
+        --edge)
+            EDGE="true"
+        ;;
+        --vimnano) 
+            VIM_NANO="true"
+        ;;
+        --pop)
+            POP="true"
+        ;;
+    esac
+done
+
 # Method Declaration
 install_apps() {
 
@@ -27,13 +60,13 @@ install_apps() {
     flatpak install flathub com.discordapp.Discord flathub org.gimp.GIMP -y flathub org.videolan.VLC flathub io.github.shiftey.Desktop flathub org.onlyoffice.desktopeditors -y
 
     #Optional
-    if [[$SPOTIFY == "true"]]; then
+    if [["$SPOTIFY" == "true"]]; then
         flatpak install flathub com.spotify.Client -y
     fi
-    if [[$POSTMAN == "true"]]; then
+    if [["$POSTMAN" == "true"]]; then
         sudo -S flatpak install flathub com.getpostman.Postman -y 
     fi
-    if [[$THUNDERBIRD == "true"]]; then
+    if [["$THUNDERBIRD" == "true"]]; then
         sudo -S flatpak install flathub org.mozilla.Thunderbird -y
     fi
 
@@ -72,14 +105,14 @@ install_apps() {
 
     # Browser
     # Chrome
-    if [[$CHROME == "true"]]; then 
+    if [["$CHROME" == "true"]]; then 
         echo $PASSWORD | sudo -S dnf install fedora-workstation-repositories
         echo $PASSWORD | sudo -S dnf config-manager --set-enabled google-chrome
         echo $PASSWORD | sudo -S dnf install google-chrome-stable -y
     fi
 
     # Microsoft Edge
-    if [[$EDGE == "true"]]; then 
+    if [["$EDGE" == "true"]]; then 
         echo $PASSWORD | sudo -S dnf install dnf-plugins-core -y
         echo $PASSWORD | sudo -S dnf config-manager --add-repo https://packages.microsoft.com/yumrepos/edge
         echo $PASSWORD | sudo -S dnf update --refresh -y
@@ -87,12 +120,12 @@ install_apps() {
     fi
 
     # Vim & Nano
-    if [[$VIM_NANO == "true"]]; then 
+    if [["$VIM_NANO" == "true"]]; then 
         echo $PASSWORD | sudo -S dnf install gvim nano -y
     fi 
 
     # Pop Shell
-    if [[$POP == "true"]]; then 
+    if [["$POP" == "true"]]; then 
         echo $PASSWORD | sudo -S dnf install cargo rust gtk3-devel gnome-shell-extension-pop-shell -y 
         git clone https://github.com/pop-os/shell-shortcuts
         cd shell-shortcuts
@@ -107,44 +140,15 @@ install_apps() {
     cd jetbrains-toolbox-1.23.11731
     chmod +x jetbrains-toolbox
     ./jetbrains-toolbox &
+    disown
+
 
 
     echo "Installing GNOME extensions..."
     echo "Done!"
 }
 
-# echo $PASSWORD | sudo -S password input
-
-echo "Enter sudo password:"
-read -s PASSWORD
-
-# Command line arguments processing
-
-for var in "$@"; do
-    case "$var" in 
-        --spotify) 
-            SPOTIFY="true"
-        ;;
-        --postman) 
-            POSTMAN="true"
-        ;;
-        --thunderbird) 
-            THUNDERBIRD="true"
-        ;;
-        --chrome)
-            CHROME="true"
-        ;;
-        --edge)
-            EDGE="true"
-        ;;
-        --vimnano) 
-            VIM_NANO="true"
-        ;;
-        --pop)
-            POP="true"
-        ;;
-    esac
-done
+#Install Comfirmation
 
 echo "You're about to install the following applications / extensions:"
 echo "- KeePassXC"
@@ -159,31 +163,31 @@ echo "- GitHub CLI"
 echo "- OnlyOffice"
 echo "- Heroic Games Launcher"
 
-if [[ $SPOTIFY == "true" ]]; then
+if [[ "$SPOTIFY" == "true" ]]; then
     echo "- Spotify"
 fi
 
-if [[ $POSTMAN == "true" ]]; then
+if [[ "$POSTMAN" == "true" ]]; then
     echo "- Postman"
 fi
 
-if [[ $THUNDERBIRD == "true" ]]; then
+if [[ "$THUNDERBIRD" == "true" ]]; then
     echo "- Thunderbird"
 fi
 
-if [[ $CHROME == "true" ]]; then
+if [[ "$CHROME" == "true" ]]; then
     echo "- Google Chrome"
 fi
 
-if [[ $EDGE == "true" ]]; then
+if [[ "$EDGE" == "true" ]]; then
     echo "- Microsoft Edge"
 fi
 
-if [[ $VIM_NANO == "true" ]]; then
+if [[ "$VIM_NANO" == "true" ]]; then
     echo "- Vim & Nano"
 fi
 
-if [[ $POP == "true" ]]; then
+if [[ "$POP" == "true" ]]; then
     echo "- Pop! Shell"
 fi
 
